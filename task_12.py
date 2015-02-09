10fc2d9451c0ef0df97d8314d976f6ca74358806
xiangyq <youqing.xiang@spsmail.cuny.edu>
2015-02-09 15:23:27 +0000

    added task_12.py

---
+++ task_12.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Numeric types python file """
+
+import decimal, fractions
+
+
+INVAL = 1
+
+FLOATVAL = 0.1
+
+DECVAL = decimal.Decimal('0.1')
+
+FRACVAL = fractions.Fraction(1,10)
