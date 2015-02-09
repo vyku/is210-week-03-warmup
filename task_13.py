6331ebbec2e9f202e4641934ce8d88dcd2f98637
xiangyq <youqing.xiang@spsmail.cuny.edu>
2015-02-09 15:44:32 +0000

    added task_13.py

--- task_13.py
+++ task_13.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Imports values from task 13 to test equality.
-
-.. hint::
-
-    You can access task_12 data in the following example type:
-
-.. code:: python
-
-    print task_12.FLOATVAL
-"""
+""" Testing equality file """
 
 import task_12
+
+FRAC_DEC_EQUAL = task_12.DECVAL == task_12.FRACVAL
+
+DEC_FLOAT_INEQUAL = task_12.FLOATVAL != task_12.DECVAL
