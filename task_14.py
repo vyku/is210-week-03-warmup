25912a7db99cfc45daa8d110680be9c727cd0380
xiangyq <youqing.xiang@spsmail.cuny.edu>
2015-02-09 15:53:20 +0000

    added task_14.py

---
+++ task_14.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Another comparing python file """
+
+IS_TRUE = True
+
+IS_FALSE = False
+
+IS_NONE = None
+
+INTEGER_EQUIV = IS_TRUE == 1 and IS_FALSE == 0
