2c2a18fc69d95c3716be6682f894d0db4825960f
xiangyq <youqing.xiang@spsmail.cuny.edu>
2015-02-09 15:04:33 +0000

    changed task_09.py

--- task_09.py
+++ task_09.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A computer that can build the question."""
+"""A computer that can build the question.
+   A computer that also can answere the question.
+"""
+
 
 THE_ANSWER_TO_EVERYTHING = 42
 
