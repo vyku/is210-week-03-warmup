e1e7726d2ec0e1ebc693ddb24c23900cfa199150
xiangyq <youqing.xiang@spsmail.cuny.edu>
2015-02-10 03:18:29 +0000

    task_09.py modified

--- task_09.py
+++ task_09.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """A computer that can build the question. A computer that also can answere the
-   question. I like computer for what they are!
+   question. I like computer for what they are! I wish I could sit in front of
+   computer for a while day with a cup of coffee!
 """
 
 
