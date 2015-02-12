e198c435d9be7a023b344c89cf5216c022089830
vyku visva <vykunthan.visvalingam@spsmail.cuny.edu>
2015-02-12 02:37:15 +0000

    "commiting task05"

--- task_05.py
+++ task_05.py
@@ -3,5 +3,10 @@
 """Splinter would be proud."""
 
 
-TEENAGE_MUTANT_NINJAS = ('Michaelangelo. Leonardo. Rafael. Donatello. Heroes '
-                         'in a half shell.')
+TEENAGE_MUTANT_NINJAS = ('Michaelangelo. Leonardo. Rafael. Donatello. Heroes
+                         in a half shell'.)
+
+TEENAGE_MUTANT_NINJAS.split()
+'
+
+print TEENAGE_MUTANT_NINJAS 
